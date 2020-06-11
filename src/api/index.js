ba4b3de7fba3a892a75310b280bb627820e59067
shandong115 <mr.danzhao@163.com>
2020-06-11 12:11:50 +0800

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -13,6 +13,6 @@ import axios from 'axios'
 export function getOnePageBooks (currentPage) {
   return axios({
     method: 'get',
-    url: 'http://192.168.31.42:3080/pageBooks?p=' + currentPage
+    url: 'http://192.168.31.42:3080/onePageBooks?p=' + currentPage
   })
 }
