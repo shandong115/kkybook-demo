9287d6b8225391dbfa5bf517d323cb5cd8503e8e
shandong115 <mr.danzhao@163.com>
2020-06-18 23:23:39 +0800

    ...

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+  devServer: {
+    /*
+    before(app) {
+      mock(app, '/book/home', homeData)
+      mock(app, '/book/shelf', shelfData)
+      mock(app, '/book/list', listData)
+      mock(app, '/book/flat-list', flatListData)
+    }
+    */
+  },
+  configureWebpack: {
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 524288 * 10,
+      maxEntrypointSize: 524288 * 10
+    }
+  }
+}
